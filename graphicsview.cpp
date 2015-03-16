ed35e439d87ebc9ec147d264e789c279d25815f6
lellisls <lucaslellis777@gmail.com>
2015-03-15 21:06:28 -0300

    Align to grid

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -3,56 +3,55 @@
 #include "graphicsview.h"
 
 GraphicsView::GraphicsView(QWidget *parent) :
-    QGraphicsView(parent)
-{
-    setAcceptDrops(true);
+  QGraphicsView(parent) {
+  setAcceptDrops(true);
 }
 
-void GraphicsView::dragEnterEvent(QDragEnterEvent *event)
-{
-    if(event->mimeData()->hasFormat("application/x-dnditemdata")){
-        if(event->source() == this){
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-        }else{
-            event->acceptProposedAction();
-        }
-    }else{
-        event->ignore();
+void GraphicsView::dragEnterEvent(QDragEnterEvent *event) {
+  if(event->mimeData()->hasFormat("application/x-dnditemdata")) {
+    if(event->source() == this) {
+      event->setDropAction(Qt::MoveAction);
+      event->accept();
+    } else {
+      event->acceptProposedAction();
     }
+  } else {
+    event->ignore();
+  }
 }
 
-void GraphicsView::dragMoveEvent(QDragMoveEvent *event)
-{
-    if(event->mimeData()->hasFormat("application/x-dnditemdata")){
-        if(event->source() == this){
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-        }else{
-            event->acceptProposedAction();
-        }
-    }else{
-        event->ignore();
+void GraphicsView::dragMoveEvent(QDragMoveEvent *event) {
+  if(event->mimeData()->hasFormat("application/x-dnditemdata")) {
+    if(event->source() == this) {
+      event->setDropAction(Qt::MoveAction);
+      event->accept();
+    } else {
+      event->acceptProposedAction();
     }
+  } else {
+    event->ignore();
+  }
 }
 
-void GraphicsView::dropEvent(QDropEvent *event)
-{
-    qDebug() << "Drop";
-    if(event->mimeData()->hasFormat("application/x-dnditemdata")){
-        qDebug() << "Drop Accepted";
-        QByteArray itemData = event->mimeData()->data("application/x-dnditemdata");
-        QDataStream dataStream(&itemData, QIODevice::ReadOnly);
+void GraphicsView::dropEvent(QDropEvent *event) {
+  qDebug() << "Drop";
+  if(event->mimeData()->hasFormat("application/x-dnditemdata")) {
+    qDebug() << "Drop Accepted";
+    QByteArray itemData = event->mimeData()->data("application/x-dnditemdata");
+    QDataStream dataStream(&itemData, QIODevice::ReadOnly);
 
-        QPixmap pixmap;
-        QPoint offset;
-        dataStream >> pixmap >> offset;
+    QPixmap pixmap;
+    QPoint offset;
+    dataStream >> pixmap >> offset;
 
-        QLabel *newIcon = new QLabel(this);
-        newIcon->setPixmap(pixmap);
-        newIcon->move(event->pos() - offset);
-        newIcon->show();
-        newIcon->setAttribute(Qt::WA_DeleteOnClose);
+    QPoint pos = event->pos();
+    pos = QPoint( 64 * (pos.x()/64), 64 * (pos.y()/64));
+
+    QLabel *newIcon = new QLabel(this);
+    newIcon->setPixmap(pixmap);
+    newIcon->move(pos);
+    newIcon->show();
+    newIcon->setAttribute(Qt::WA_DeleteOnClose);
 
 //        QGraphicsProxyWidget *proxy = scene()->addWidget(newIcon);
 //        proxy->setFlag(QGraphicsItem::ItemIsMovable);
@@ -67,50 +66,49 @@ void GraphicsView::dropEvent(QDropEvent *event)
 //        item->setFlag(QGraphicsPixmapItem::ItemIsMovable);
 //        item->setFlag(QGraphicsPixmapItem::ItemIsSelectable);
 
-        if(event->source() == this){
-            event->setDropAction(Qt::MoveAction);
-            event->accept();
-        }else{
-            event->acceptProposedAction();
-        }
-    }else{
-        event->ignore();
+    if(event->source() == this) {
+      event->setDropAction(Qt::MoveAction);
+      event->accept();
+    } else {
+      event->acceptProposedAction();
     }
+  } else {
+    event->ignore();
+  }
 }
 
-void GraphicsView::mousePressEvent(QMouseEvent *event)
-{
+void GraphicsView::mousePressEvent(QMouseEvent *event) {
 
-    QGraphicsView::mousePressEvent(event);
-    //    if(itemAt(event->pos())){
-    //        QPixmap pixmap = itemAt(event->pos())
-    //        QPixmap pixmap = *this->pixmap();
+  QGraphicsView::mousePressEvent(event);
+  //    if(itemAt(event->pos())){
+  //        QPixmap pixmap = itemAt(event->pos())
+  //        QPixmap pixmap = *this->pixmap();
 
-    //        QByteArray itemData;
-    //        QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-    //        dataStream << pixmap << QPoint(event->pos());
+  //        QByteArray itemData;
+  //        QDataStream dataStream(&itemData, QIODevice::WriteOnly);
+  //        dataStream << pixmap << QPoint(event->pos());
 
-    //        QMimeData *mimeData = new QMimeData;
-    //        mimeData->setData("application/x-dnditemdata", itemData);
+  //        QMimeData *mimeData = new QMimeData;
+  //        mimeData->setData("application/x-dnditemdata", itemData);
 
-    //        QDrag *drag = new QDrag(this);
-    //        drag->setMimeData(mimeData);
-    //        drag->setPixmap(pixmap);
-    //        drag->setHotSpot(event->pos());
+  //        QDrag *drag = new QDrag(this);
+  //        drag->setMimeData(mimeData);
+  //        drag->setPixmap(pixmap);
+  //        drag->setHotSpot(event->pos());
 
-    //        QPixmap tempPixmap = pixmap;
-    //        QPainter painter;
-    //        painter.begin(&tempPixmap);
-    //        painter.fillRect(pixmap.rect(), QColor(127, 127, 127, 127));
-    //        painter.end();
+  //        QPixmap tempPixmap = pixmap;
+  //        QPainter painter;
+  //        painter.begin(&tempPixmap);
+  //        painter.fillRect(pixmap.rect(), QColor(127, 127, 127, 127));
+  //        painter.end();
 
-    //        setPixmap(tempPixmap);
+  //        setPixmap(tempPixmap);
 
-    //        if(drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction){
-    //            close();
-    //        } else{
-    //            show();
-    //            setPixmap(pixmap);
-    //        }
-    //    }
+  //        if(drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction){
+  //            close();
+  //        } else{
+  //            show();
+  //            setPixmap(pixmap);
+  //        }
+  //    }
 }
