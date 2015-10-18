b220dd1307a5eee5171bf5679139ca6e62e4166a
Lucas <Lucaslellis777@gmail.com>
2015-10-18 14:33:01 -0200

    Fixed drag cursor

--- app/graphicsview.cpp
+++ app/graphicsview.cpp
@@ -1,42 +1,43 @@
 #include "graphicsview.h"
 
+#include <QApplication>
 #include <QKeyEvent>
 #include <QScrollBar>
 
-GraphicsView::GraphicsView(QWidget * parent) : QGraphicsView(parent) {
+GraphicsView::GraphicsView( QWidget *parent ) : QGraphicsView( parent ) {
   _pan = false;
 }
 
-void GraphicsView::mousePressEvent(QMouseEvent * e) {
-  if(e->button() == Qt::MiddleButton) {
+void GraphicsView::mousePressEvent( QMouseEvent *e ) {
+  if( e->button( ) == Qt::MiddleButton ) {
     _pan = true;
-    _panStartX = e->x();
-    _panStartY = e->y();
-    setCursor(Qt::ClosedHandCursor);
-    e->accept();
+    _panStartX = e->x( );
+    _panStartY = e->y( );
+    QApplication::setOverrideCursor( Qt::ClosedHandCursor );
+    e->accept( );
     return;
   }
-  QGraphicsView::mousePressEvent(e);
+  QGraphicsView::mousePressEvent( e );
 }
 
-void GraphicsView::mouseReleaseEvent(QMouseEvent * e) {
-  if(e->button() == Qt::MiddleButton) {
+void GraphicsView::mouseReleaseEvent( QMouseEvent *e ) {
+  if( e->button( ) == Qt::MiddleButton ) {
     _pan = false;
-    setCursor(Qt::ArrowCursor);
-    e->accept();
+    QApplication::restoreOverrideCursor();
+    e->accept( );
     return;
   }
-  QGraphicsView::mouseReleaseEvent(e);
+  QGraphicsView::mouseReleaseEvent( e );
 }
 
-void GraphicsView::mouseMoveEvent(QMouseEvent *e) {
-  if(_pan) {
-    horizontalScrollBar()->setValue(horizontalScrollBar()->value() - (e->x() - _panStartX));
-    verticalScrollBar()->setValue(verticalScrollBar()->value() - (e->y() - _panStartY));
-    _panStartX = e->x();
-    _panStartY = e->y();
-    e->accept();
+void GraphicsView::mouseMoveEvent( QMouseEvent *e ) {
+  if( _pan ) {
+    horizontalScrollBar( )->setValue( horizontalScrollBar( )->value( ) - ( e->x( ) - _panStartX ) );
+    verticalScrollBar( )->setValue( verticalScrollBar( )->value( ) - ( e->y( ) - _panStartY ) );
+    _panStartX = e->x( );
+    _panStartY = e->y( );
+    e->accept( );
     return;
   }
-  QGraphicsView::mouseMoveEvent(e);
+  QGraphicsView::mouseMoveEvent( e );
 }
