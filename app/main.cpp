a0527a743bd24e95b3a8ad68e45d742098f9c9f8
Lucas <Lucaslellis777@gmail.com>
2015-10-18 17:29:24 -0200

    Software version updated.

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setOrganizationName("WPanda");
   a.setApplicationName("WiredPanda");
-  a.setApplicationVersion("1.4");
+  a.setApplicationVersion("1.5");
   MainWindow w;
   w.show();
   if(argc == 2 ) {
