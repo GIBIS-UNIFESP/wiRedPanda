32b7cb8ce3d06ecb330a1728f85ebf11fbfbb2e6
Lucas <Lucaslellis777@gmail.com>
2016-02-20 12:35:03 -0200

    Jquery Animations

--- js/power.js
+++ js/power.js
@@ -1,7 +1,29 @@
+
+$('.banner').hide();
+$('.features').hide();
+$('.banner-title').hide();
+$('.banner-subtitle').hide();
 /*====================================
 =            DOM IS READY            =
 ====================================*/
+
 $(function() {
+    $('.banner').animate({
+      // width: [ "toggle", "swing" ],
+      opacity: "toggle"
+    },1000);
+
+    $('.banner-title').animate({
+      opacity: "toggle"
+    },1500);
+
+    $('.banner-subtitle').animate({
+      opacity: "toggle"
+    },2000);
+
+
+    $('.features').animate({opacity:'toggle'},1000);
+
     $('.pagination .active a').click(function() {
         return false;
     });
@@ -28,5 +50,4 @@ $(window).resize(function() {
 =            WINDOW IS SCROLLED            =
 ==========================================*/
 $(window).scroll(function() {
-
 });
