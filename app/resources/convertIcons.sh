317c98956f1dfc214ac6afe9fd91d06e0093aae8
Lucas Santana Lellis <lucaslellis777@gmail.com>
2016-03-24 11:50:56 -0300

    Panda File icon

--- app/resources/convertIcons.sh
+++ app/resources/convertIcons.sh
@@ -1,6 +1,7 @@
-find . -type f -name '*.svg' | while read file; do
-  inkscape -z $file -e ${file%.*}.png;
-done;
+# find . -type f -name '*.svg' | while read file; do
+#   inkscape -z $file -e ${file%.*}.png;
+# done;
 
-for size in [26,32,48,64,128]:
-  inkscape -z wpanda.svg -e icons/wpanda.png -h ${size};
+for size in 26 32 48 64 128; do
+  inkscape -z wpanda.svg -e icons/${size}x${size}/wpanda.png -h ${size};
+done;
