32e113792005191297904252643d673b16ac2b67
Lucas Santana Lellis <lucaslellis777@gmail.com>
2016-03-24 12:03:35 -0300

    Fileicons

--- app/resources/convertIcons.sh
+++ app/resources/convertIcons.sh
@@ -5,6 +5,5 @@ done;
 for size in 26 32 48 64 128; do
   mkdir -p icons/${size}x${size}
   inkscape -z wpanda.svg -e icons/${size}x${size}/wpanda.png -h ${size};
-  mkdir -p fileIcons/${size}x${size}
-  inkscape -z pandaFile.svg -e fileIcons/${size}x${size}/wpanda-file.png -h ${size};
+  inkscape -z pandaFile.svg -e icons/${size}x${size}/wpanda-file.png -h ${size};
 done;
