d3aa2f77b7cc5b089b9fa21eed040b80503068e8
Lucas Santana Lellis <lucaslellis777@gmail.com>
2016-04-08 10:07:55 -0300

    New QTestFiles

--- test/testfiles.cpp
+++ test/testfiles.cpp
@@ -1,30 +1,42 @@
 #include "testfiles.h"
 
+#include "globalproperties.h"
 #include <mainwindow.h>
 
 void TestFiles::init( ) {
-  w = new MainWindow( );
-  w->undoView->hide();
+  editor = new Editor( this );
 }
 
 void TestFiles::cleanup( ) {
-  w->deleteLater( );
+  delete editor;
 }
 
 void TestFiles::testFiles( ) {
-  qDebug() << "CURRENTDIR: " << CURRENTDIR;
+  qDebug( ) << "CURRENTDIR: " << CURRENTDIR;
   QDir examplesDir( QString( "%1/../examples/" ).arg( CURRENTDIR ) );
-  qDebug() << "Examples dir:" << examplesDir.absolutePath();
+  qDebug( ) << "Examples dir:" << examplesDir.absolutePath( );
   QStringList entries;
   entries << "*.panda";
   QFileInfoList files = examplesDir.entryInfoList( entries );
-  Q_ASSERT(files.size() > 0);
+  Q_ASSERT( files.size( ) > 0 );
   int counter = 0;
   for( QFileInfo f : files ) {
-    qDebug() << "File " << counter++ << " from " << files.size() << ": " << f.fileName();
-    Q_ASSERT( f.exists() );
-    Q_ASSERT( w->open( f.absoluteFilePath( ) ) );
-    QList< QGraphicsItem* > items = w->editor->getScene()->items( );
+    qDebug( ) << "File " << counter++ << " from " << files.size( ) << ": " << f.fileName( );
+    Q_ASSERT( f.exists( ) );
+    QFile pandaFile( f.filePath( ) );
+    GlobalProperties::currentFile = f.filePath( );
+    Q_ASSERT( pandaFile.exists( ) );
+    Q_ASSERT( pandaFile.open( QFile::ReadOnly ) );
+
+    QDataStream ds( &pandaFile );
+    try {
+      editor->load( ds );
+    }
+    catch( std::runtime_error &e ) {
+      QFAIL( QString( "Could not load the file! Error: %1" ).arg( QString::fromStdString( e.what( ) ) ).toUtf8( ) );
+    }
+    pandaFile.close( );
+    QList< QGraphicsItem* > items = editor->getScene( )->items( );
     for( QGraphicsItem *item : items ) {
       if( item->type( ) == QNEConnection::Type ) {
         QNEConnection *conn = qgraphicsitem_cast< QNEConnection* >( item );
@@ -34,6 +46,4 @@ void TestFiles::testFiles( ) {
       }
     }
   }
-  w->save( "test.panda" );
-  w->close( );
 }
