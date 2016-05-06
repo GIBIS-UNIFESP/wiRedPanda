3565464f194ceaad2efc688ee41d3159a0936171
Lucas Santana Lellis <lucaslellis777@gmail.com>
2016-05-06 11:33:14 -0300

    Fixed issues on portable launcher

--- installer/Linux/portable_files/shared_qt.sh
+++ installer/Linux/portable_files/shared_qt.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-appname=`basename $0 | sed s,\.sh$,,`
-
-dirname=`dirname $0`
+appname=`basename "$0" | sed s,\.sh$,,`
+dirname=`dirname "$0"`
 tmp="${dirname#?}"
-
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
+
 LD_LIBRARY_PATH=$dirname
 export LD_LIBRARY_PATH
-sed -i "s,Icon=.*,Icon=${dirname}/wpanda.svg,g" ${dirname}/../wpanda.desktop
-$dirname/$appname "$@"
+sed -i "s,Icon=.*,Icon=${dirname}/wpanda.svg,g" "$dirname/../wpanda.desktop"
+"$dirname/$appname" "$@"
