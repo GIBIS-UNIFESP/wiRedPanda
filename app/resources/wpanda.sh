3cb4c3a2da8dd83112558787f0567c6327be4979
Lucas Santana Lellis <lucaslellis777@gmail.com>
2017-04-03 14:42:48 -0300

    Deploy with Qt 5.8 and dynamic linking...

--- app/resources/wpanda.sh
+++ app/resources/wpanda.sh
@@ -1,12 +1,12 @@
  #!/bin/sh
  appname=`basename $0 | sed s,\.sh$,,`
- 
+
  dirname=`dirname $0`
  tmp="${dirname#?}"
- 
+
  if [ "${dirname%$tmp}" != "/" ]; then
  dirname=$PWD/$dirname
  fi
- LD_LIBRARY_PATH=/usr/local/lib/wpanda:${LD_LIBRARY_PATH}
- export LD_LIBRARY_PATH
+ export LD_LIBRARY_PATH=/usr/local/lib/wpanda:${LD_LIBRARY_PATH}
+ export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/local/lib/wpanda
  $dirname/$appname "$@"
