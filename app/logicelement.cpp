1da9e786a02d3e24db24478f00babaee20bf3f0a
Lucas Lellis <lelisls@github.com>
2018-02-12 12:01:50 -0200

    Added support to Node and JKFF, and Fixed crashes.

--- app/logicelement.cpp
+++ app/logicelement.cpp
@@ -74,6 +74,14 @@ bool LogicElement::getOutputValue( size_t index ) const {
   return( m_outputs.at( index ) );
 }
 
+LogicNode::LogicNode( ) : LogicElement( 1, 1 ) {
+
+}
+
+void LogicNode::_updateLogic( const std::vector< bool > &inputs ) {
+  setOutputValue( inputs[ 0 ] );
+}
+
 //LogicInput::LogicInput( Input *in ) : LogicElement( 0, 1 ), input( in ) {
 //  setOutputValue( 0, input->getOn( ) );
 //}
@@ -178,10 +186,39 @@ void LogicInput::_updateLogic( const std::vector< bool > & ) {
   // Does nothing on update
 }
 
-LogicJKFlipFlop::LogicJKFlipFlop( ) : LogicElement( 5, 2 ) {
+LogicJKFlipFlop::LogicJKFlipFlop( ) :
+  LogicElement( 5, 2 ),
+  lastClk( false ) {
 
 }
 
 void LogicJKFlipFlop::_updateLogic( const std::vector< bool > &inputs ) {
+  bool q0 = getOutputValue( 0 );
+  bool q1 = getOutputValue( 1 );
+  bool j = inputs[ 0 ];
+  bool clk = inputs[ 1 ];
+  bool k = inputs[ 2 ];
+  bool prst = inputs[ 3 ];
+  bool clr = inputs[ 4 ];
+  if( clk && !lastClk ) {
+    if( j && k ) {
+      std::swap( q0, q1 );
+    }
+    else if( j ) {
+      q0 = true;
+      q1 = false;
+    }
+    else if( k ) {
+      q0 = false;
+      q1 = true;
+    }
+  }
+  if( !prst || !clr ) {
+    q0 = !prst;
+    q1 = !clr;
+  }
+  lastClk = clk;
 
+  setOutputValue( 0, q0 );
+  setOutputValue( 1, q1 );
 }
