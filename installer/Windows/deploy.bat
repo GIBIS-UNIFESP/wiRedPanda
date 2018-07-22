2184741eec5a8a432d40264761a34253453f7533
Lucas Santana Lellis <lucaslellis777@gmail.com>
2018-07-22 13:55:32 -0300

    Windows deploy scripts updated

--- installer/Windows/deploy.bat
+++ installer/Windows/deploy.bat
@@ -1,12 +1,21 @@
 SET PATH=%PATH%;C:\Qt\Tools\mingw530_32\bin;C:\Qt\5.8\mingw53_32\bin;C:\Program Files\7-Zip;C:\Program Files (x86)\NSIS\
+del WiredPanda_*
+
 if not exist Build mkdir Build
 cd Build
 qmake.exe ..\..\..\app\
 mingw32-make.exe -j4
 cd ..
-"windeployqt.exe" "Build\release\wpanda.exe"
-"makensis.exe" wpanda.nsi
-7z.exe x wpanda_32.exe -o"%cd%\wpanda_32"
-7z.exe a -tzip wpanda_portable.zip "%cd%\wpanda_32"
+
+windeployqt.exe "Build\release\wpanda.exe"
+makensis.exe "wpanda.nsi"
+7z.exe x WiredPanda_*.exe -o"%cd%\wpanda_32"
+
+FOR /F "tokens=*" %%i IN ( 'DIR /b /a-d WiredPanda_*.exe') DO (SET EXEFILE=%%i)
+ECHO %EXEFILE%
+set OUTFILE=%EXEFILE:Windows_Installer_x86_64.exe=Windows_Portable_x86_64.zip%
+
+7z.exe a -tzip %OUTFILE% "%cd%\wpanda_32"
 RMDIR /S /Q "%cd%\wpanda_32"
+
 pause
