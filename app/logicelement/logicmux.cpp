43f973c7bfd2fba27ac45e686af9c44eba1b7d8a
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 12:20:17 -0300

    logicmux: Simplify boolean expressions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/logicelement/logicmux.cpp
+++ app/logicelement/logicmux.cpp
@@ -10,7 +10,7 @@ void LogicMux::_updateLogic(const std::vector<bool> &inputs)
     bool data1 = inputs[0];
     bool data2 = inputs[1];
     bool choice = inputs[2];
-    if (choice == false) {
+    if (!choice) {
         setOutputValue(data1);
     } else {
         setOutputValue(data2);
