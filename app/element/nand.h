bcd135cea37b8d93c04197e1d22d23a4a6214db8
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 15:38:52 -0300

    nand: Remove parentheses in return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/element/nand.h
+++ app/element/nand.h
@@ -18,11 +18,11 @@ public:
 public:
     ElementType elementType() override
     {
-        return (ElementType::NAND);
+        return ElementType::NAND;
     }
     ElementGroup elementGroup() override
     {
-        return (ElementGroup::GATE);
+        return ElementGroup::GATE;
     }
     void setSkin(bool defaultSkin, QString filename) override;
 };
