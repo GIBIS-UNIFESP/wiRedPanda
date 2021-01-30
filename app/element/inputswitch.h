41a24016f1d47f61879b8b442e20d7b7e1c99fda
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 11:21:51 -0300

    inputswitch: Add missing override
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/element/inputswitch.h
+++ app/element/inputswitch.h
@@ -8,7 +8,7 @@ class InputSwitch : public GraphicElement, public Input
 {
 public:
     explicit InputSwitch(QGraphicsItem *parent = nullptr);
-    virtual ~InputSwitch() override = default;
+    ~InputSwitch() override = default;
     static int current_id_number; // Number used to create distinct labels for each instance of this element.
 
     bool on;
@@ -18,11 +18,11 @@ protected:
 
     /* GraphicElement interface */
 public:
-    virtual ElementType elementType() override
+    ElementType elementType() override
     {
         return (ElementType::SWITCH);
     }
-    virtual ElementGroup elementGroup() override
+    ElementGroup elementGroup() override
     {
         return (ElementGroup::INPUT);
     }
@@ -31,8 +31,8 @@ public:
 public:
     void save(QDataStream &ds) const override;
     void load(QDataStream &ds, QMap<quint64, QNEPort *> &portMap, double version) override;
-    virtual bool getOn() const override;
-    virtual void setOn(bool value) override;
+    bool getOn() const override;
+    void setOn(bool value) override;
     void setSkin(bool defaultSkin, QString filename) override;
 };
 
