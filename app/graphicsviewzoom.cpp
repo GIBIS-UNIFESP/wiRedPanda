0fe08647d21f8874e2ae144f8f0c054d2da8a29d
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 11:31:14 -0300

    graphicsviewzoom: Use auto when possible
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/graphicsviewzoom.cpp
+++ app/graphicsviewzoom.cpp
@@ -94,14 +94,14 @@ bool GraphicsViewZoom::canZoomOut()
 bool GraphicsViewZoom::eventFilter(QObject *object, QEvent *event)
 {
     if (event->type() == QEvent::MouseMove) {
-        QMouseEvent *mouse_event = static_cast<QMouseEvent *>(event);
+        auto *mouse_event = static_cast<QMouseEvent *>(event);
         QPointF delta = target_viewport_pos - mouse_event->pos();
         if ((qAbs(delta.x()) > 5) || (qAbs(delta.y()) > 5)) {
             target_viewport_pos = mouse_event->pos();
             target_scene_pos = _view->mapToScene(mouse_event->pos());
         }
     } else if (event->type() == QEvent::Wheel) {
-        QWheelEvent *wheel_event = static_cast<QWheelEvent *>(event);
+        auto *wheel_event = static_cast<QWheelEvent *>(event);
         if (QApplication::keyboardModifiers() == _modifiers) {
             // if( wheel_event->orientation( ) == Qt::Vertical ) {
             double angle = wheel_event->angleDelta().y();
