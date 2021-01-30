f645b5eed7b5b895d454b28c9c14420bc8914c65
Carlos Alves <cbc.alves@gmail.com>
2021-01-30 15:41:46 -0300

    srfliflop: Adding braces

--- app/element/srflipflop.cpp
+++ app/element/srflipflop.cpp
@@ -47,9 +47,10 @@ void SRFlipFlop::updatePorts()
 
 void SRFlipFlop::setSkin(bool defaultSkin, QString filename)
 {
-    if (defaultSkin)
+    if (defaultSkin) {
         pixmapSkinName[0] = ":/memory/SR-flipflop.png";
-    else
+    } else {
         pixmapSkinName[0] = filename;
+    }
     setPixmap(pixmapSkinName[0]);
 }
