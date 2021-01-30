4dd6e160dfdd0e5e6e6b01dcfd05834457f44f67
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 15:38:52 -0300

    or: Remove parentheses in return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/element/or.h
+++ app/element/or.h
@@ -18,11 +18,11 @@ public:
 public:
     ElementType elementType() override
     {
-        return (ElementType::OR);
+        return ElementType::OR;
     }
     ElementGroup elementGroup() override
     {
-        return (ElementGroup::GATE);
+        return ElementGroup::GATE;
     }
     void setSkin(bool defaultSkin, QString filename) override;
 };
