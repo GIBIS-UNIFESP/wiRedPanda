2542029ff1c1044b4e70d39eee12b3afd0167611
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 15:38:52 -0300

    not: Remove parentheses in return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/element/not.h
+++ app/element/not.h
@@ -18,11 +18,11 @@ public:
 public:
     ElementType elementType() override
     {
-        return (ElementType::NOT);
+        return ElementType::NOT;
     }
     ElementGroup elementGroup() override
     {
-        return (ElementGroup::GATE);
+        return ElementGroup::GATE;
     }
     void setSkin(bool defaultSkin, QString filename) override;
 };
