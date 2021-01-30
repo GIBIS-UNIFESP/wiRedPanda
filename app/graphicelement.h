0b7c48aaae82b842ce25604e00b068a02d4ed706
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 11:21:51 -0300

    graphicelement: Add missing override
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/graphicelement.h
+++ app/graphicelement.h
@@ -90,14 +90,14 @@ public:
 
     /* QGraphicsItem interface */
 public:
-    int type() const
+    int type() const override
     {
         return (Type);
     }
 
-    virtual QRectF boundingRect() const;
+    QRectF boundingRect() const override;
 
-    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
     QNEPort *addPort(const QString &name, bool isOutput, int flags = 0, int ptr = 0);
 
@@ -217,7 +217,7 @@ protected:
     void setBottomPosition(int bottomPosition);
 
     /*  virtual void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *e); */
-    QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
 
 private:
     QGraphicsTextItem *m_label;
