78b5b1433c7a40267dd573ef34e073747afbfc89
Patrick José Pereira <patrickelectric@gmail.com>
2021-01-30 15:38:52 -0300

    jkflipflop: Remove parentheses in return
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/element/jkflipflop.h
+++ app/element/jkflipflop.h
@@ -20,11 +20,11 @@ public:
 public:
     ElementType elementType() override
     {
-        return (ElementType::JKFLIPFLOP);
+        return ElementType::JKFLIPFLOP;
     }
     ElementGroup elementGroup() override
     {
-        return (ElementGroup::MEMORY);
+        return ElementGroup::MEMORY;
     }
     void updatePorts() override;
     void setSkin(bool defaultSkin, QString filename) override;
