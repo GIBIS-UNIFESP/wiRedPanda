5ca1b4201451b63b4b093ee3fae13ab69fd71f38
Tomaz Canabrava <tcanabrava@kde.org>
2021-02-13 12:01:53 +0000

    Fix order of parameters on GLOB_RECURSE

--- cmake/Format.cmake
+++ cmake/Format.cmake
@@ -3,7 +3,8 @@ include(KDEClangFormat)
 file(
     GLOB_RECURSE
     ALL_CLANG_FORMAT_SOURCE_FILES
-        *.cpp *.h
+    RELATIVE
     ${PROJECT_SOURCE_DIR}
+    *.cpp *.h
 )
 kde_clang_format(${ALL_CLANG_FORMAT_SOURCE_FILES})
