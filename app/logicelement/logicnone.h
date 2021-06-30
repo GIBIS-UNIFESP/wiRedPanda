636ef582e14fd3bf08b385a48f892efdf7840832
Fabio Cappabianco <fcappabianco@gmail.com>
2021-06-30 15:38:00 -0300

    None logic element

---
+++ app/logicelement/logicnone.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 - 2021, GIBIS-Unifesp and the wiRedPanda contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef LOGICNONE_H
+#define LOGICNONE_H
+
+#include "logicelement.h"
+
+class LogicNone : public LogicElement
+{
+  /* LogicElement interface */
+public:
+  explicit LogicNone();
+protected:
+    void _updateLogic(const std::vector<bool> &inputs) override;
+};
+
+#endif // LOGICNONE_H
