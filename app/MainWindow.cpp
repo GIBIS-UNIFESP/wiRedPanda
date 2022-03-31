b446d474de9adc1b90ffe0dfc990da8cc2884ce3
liomar <liomarhora@gmail.com>
2022-03-31 16:56:52 -0300

    removing the automatic methods and placing connect in the constructor of the mainWindow class

--- app/MainWindow.cpp
+++ app/MainWindow.cpp
@@ -726,11 +726,16 @@ void MainWindow::updateRecentICs()
 
 QString MainWindow::getOpenICFile()
 {
-    return QFileDialog::getOpenFileName(this, tr("Open File as IC"), defaultDirectory.absolutePath(), tr("Panda files (*.panda)"));
+    return QFileDialog::getOpenFileName(this, tr("Load File as IC"), defaultDirectory.absolutePath(), tr("Panda files (*.panda)"));
 }
 
 
+<<<<<<< HEAD:app/MainWindow.cpp
     ui->statusBar->showMessage(tr("Loaded ic successfully."), 2000);
+=======
+    ui->statusBar->showMessage(tr("Loaded IC successfully."), 2000);
+}
+>>>>>>> 252ae85 (Completed translation to Portuguese):app/mainwindow.cpp
 
 
 
@@ -832,7 +837,7 @@ bool MainWindow::exportToArduino(QString fname)
 
         qDebug() << "Arduino code successfully generated.";
     } catch (std::runtime_error &e) {
-        QMessageBox::warning(this, tr("Error"), tr("<strong>Error while exporting to arduino code:</strong><br>%1").arg(e.what()));
+        QMessageBox::warning(this, tr("Error"), tr("<strong>Error while exporting to Arduino code:</strong><br>%1").arg(e.what()));
         return false;
     }
 
