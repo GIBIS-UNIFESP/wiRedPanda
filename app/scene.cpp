d48dc9ade68133e3478b205f7dee3f12b1fedde2
Rodrigo Torres <torres.dark@gmail.com>
2022-05-20 12:04:36 -0300

    Update translation

--- app/scene.cpp
+++ app/scene.cpp
@@ -34,11 +34,11 @@ Scene::Scene(QObject *parent)
     m_selectionRect.setFlag(QGraphicsItem::ItemIsSelectable, false);
     addItem(&m_selectionRect);
 
-    m_undoAction = undoStack()->createUndoAction(this, "&Undo");
+    m_undoAction = undoStack()->createUndoAction(this, tr("&Undo"));
     m_undoAction->setIcon(QIcon(":/toolbar/undo.png"));
     m_undoAction->setShortcut(QKeySequence::Undo);
 
-    m_redoAction = undoStack()->createRedoAction(this, "&Redo");
+    m_redoAction = undoStack()->createRedoAction(this, tr("&Redo"));
     m_redoAction->setIcon(QIcon(":/toolbar/redo.png"));
     m_redoAction->setShortcut(QKeySequence::Redo);
 
