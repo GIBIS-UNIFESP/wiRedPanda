e1928938668ae8e6b01099351f9704f9219cf8cc
Rodrigo Torres <torres.dark@gmail.com>
2022-06-11 14:11:42 -0300

    Refactor

--- app/elementlabel.cpp
+++ app/elementlabel.cpp
@@ -23,16 +23,12 @@ ElementLabel::ElementLabel(const QPixmap &pixmap, ElementType type, const QStrin
     , m_pixmap(pixmap)
     , m_icFileName(icFileName)
 {
-    QString name_ = ElementFactory::translatedName(type);
-    if (type == ElementType::IC) {
-        name_ = QFileInfo(icFileName).baseName().toUpper();
-    }
-
     m_iconLabel.setPixmap(pixmap);
     m_iconLabel.setScaledContents(true);
     m_iconLabel.setFixedSize(64, 64);
 
-    m_nameLabel.setText(name_);
+    m_nameLabel.setText((type == ElementType::IC) ? QFileInfo(icFileName).baseName().toUpper()
+                                                  : ElementFactory::translatedName(type));
 
     auto *itemLayout = new QHBoxLayout();
     itemLayout->setSpacing(6);
@@ -50,8 +46,7 @@ ElementLabel::ElementLabel(const QPixmap &pixmap, ElementType type, const QStrin
 void ElementLabel::updateName()
 {
     if (m_elementType != ElementType::IC) {
-        QString name_ = ElementFactory::translatedName(m_elementType);
-        m_nameLabel.setText(name_);
+        m_nameLabel.setText(ElementFactory::translatedName(m_elementType));
     }
 }
 
@@ -80,33 +75,33 @@ void ElementLabel::startDrag()
 {
     QPixmap pixMap = pixmap();
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
-    QPoint pos = m_iconLabel.pixmap().rect().center();
+    QPoint offset = m_iconLabel.pixmap().rect().center();
 #else
-    QPoint pos = m_iconLabel.pixmap()->rect().center();
+    QPoint offset = m_iconLabel.pixmap()->rect().center();
 #endif
     QByteArray itemData;
     QDataStream stream(&itemData, QIODevice::WriteOnly);
-    stream << pos << m_elementType << m_icFileName;
+    stream << offset << m_elementType << m_icFileName;
 
     auto *mimeData_ = new QMimeData;
     mimeData_->setData("application/x-dnditemdata", itemData);
     auto *drag = new QDrag(parent());
     drag->setMimeData(mimeData_);
     drag->setPixmap(pixMap);
-    drag->setHotSpot(pos);
+    drag->setHotSpot(offset);
     drag->exec(Qt::CopyAction, Qt::CopyAction);
 }
 
 QMimeData *ElementLabel::mimeData()
 {
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
-    QPoint pos = m_iconLabel.pixmap().rect().center();
+    QPoint offset = m_iconLabel.pixmap().rect().center();
 #else
-    QPoint pos = m_iconLabel.pixmap()->rect().center();
+    QPoint offset = m_iconLabel.pixmap()->rect().center();
 #endif
     QByteArray itemData;
     QDataStream stream(&itemData, QIODevice::WriteOnly);
-    stream << pos << m_elementType << m_icFileName;
+    stream << offset << m_elementType << m_icFileName;
 
     auto *mimeData = new QMimeData;
     mimeData->setData("application/x-dnditemdata", itemData);
