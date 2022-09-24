31b567f61a993ceada4fe0db16ad4decb106d6ea
Rodrigo Torres <torres.dark@gmail.com>
2022-09-24 20:28:53 -0300

    Button to remove ICs

---
+++ app/trashbutton.cpp
@@ -0,0 +1,46 @@
+#include "trashbutton.h"
+
+#include "enums.h"
+
+#include <QDragEnterEvent>
+#include <QMessageBox>
+#include <QMimeData>
+
+TrashButton::TrashButton(QWidget *parent) : QPushButton(parent)
+{
+    setAcceptDrops(true);
+}
+
+void TrashButton::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasFormat("wpanda/x-dnditemdata")) {
+        event->acceptProposedAction();
+    }
+}
+
+void TrashButton::dropEvent(QDropEvent *event)
+{
+    if (event->mimeData()->hasFormat("wpanda/x-dnditemdata")) {
+        QMessageBox msgBox;
+        msgBox.setParent(this);
+        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+        msgBox.setText(tr("File will be deleted. Are you sure?"));
+        msgBox.setWindowModality(Qt::WindowModal);
+        msgBox.setDefaultButton(QMessageBox::No);
+
+        if (msgBox.exec() == QDialog::Rejected) {
+            event->setAccepted(false);
+            return;
+        }
+
+        QByteArray itemData = event->mimeData()->data("wpanda/x-dnditemdata");
+        QDataStream stream(&itemData, QIODevice::ReadOnly);
+        stream.setVersion(QDataStream::Qt_5_12);
+
+        QPoint offset;      stream >> offset;
+        ElementType type;   stream >> type;
+        QString icFileName; stream >> icFileName;
+
+        emit removeICFile(icFileName);
+    }
+}
