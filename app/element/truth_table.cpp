e0f334f9a4ba34fb052c6a85af0b884208a0ec6b
mams76 <maycon.andre@unifesp.br>
2023-12-04 08:26:02 -0300

    Created TruthTable Element, based on AND

---
+++ app/element/truth_table.cpp
@@ -0,0 +1,25 @@
+// Copyright 2015 - 2022, GIBIS-UNIFESP and the WiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "truth_table.h"
+
+#include "globalproperties.h"
+
+namespace
+{
+    int id = qRegisterMetaType<TruthTable>();
+}
+
+TruthTable::TruthTable(QGraphicsItem *parent)
+    : GraphicElement(ElementType::And, ElementGroup::Gate, ":/basic/and.svg", tr("TruthTable"), tr("TruthTable"), 2, 8, 1, 1, parent)
+{
+    if (GlobalProperties::skipInit) {
+        return;
+    }
+
+    m_defaultSkins << m_pixmapPath;
+    m_alternativeSkins = m_defaultSkins;
+    setPixmap(0);
+
+    setCanChangeSkin(true);
+}
---
+++ app/element/truth_table.h
@@ -0,0 +1,16 @@
+// Copyright 2015 - 2022, GIBIS-UNIFESP and the WiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "graphicelement.h"
+
+class TruthTable : public GraphicElement
+{
+    Q_OBJECT
+
+public:
+    explicit TruthTable(QGraphicsItem *parent = nullptr);
+};
+
+Q_DECLARE_METATYPE(TruthTable)
