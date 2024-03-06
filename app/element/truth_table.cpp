d1c8d0d8490442fb1a3009571f7e3c60c7d432b4
Matheus <matheus.resteves@gmail.com>
2024-03-06 15:26:46 -0300

    rotate the truth table icon

--- app/element/truth_table.cpp
+++ app/element/truth_table.cpp
@@ -11,7 +11,7 @@ namespace
 }
 
 TruthTable::TruthTable(QGraphicsItem *parent)
-    : GraphicElement(ElementType::TruthTable, ElementGroup::IC, ":/basic/truthtable.svg", tr("TruthTable"), tr("TruthTable"), 2, 8, 1, 8, parent)
+    : GraphicElement(ElementType::TruthTable, ElementGroup::IC, ":/basic/truthtable-rotated.svg", tr("TruthTable"), tr("TruthTable"), 2, 8, 1, 8, parent)
 {
     if (GlobalProperties::skipInit) {
         return;
@@ -23,3 +23,46 @@ TruthTable::TruthTable(QGraphicsItem *parent)
     setCanChangeSkin(true);
     setHasLabel(true);
 }
+
+void TruthTable::generatePixmap()
+{
+    // make pixmap
+    const QSize size = portsBoundingRect().united(QRectF(0, 0, 64, 64)).size().toSize();
+    QPixmap tempPixmap(size);
+    tempPixmap.fill(Qt::transparent);
+
+    QPainter tmpPainter(&tempPixmap);
+
+    tmpPainter.setBrush(QColor(126, 126, 126));
+    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
+
+    // draw package
+    QPoint topLeft = tempPixmap.rect().topLeft();
+    topLeft.setX(topLeft.x() + 7);
+    QSize finalSize = tempPixmap.rect().size();
+    finalSize.setWidth(finalSize.width() - 14);
+    QRectF finalRect = QRectF(topLeft, finalSize);
+    tmpPainter.drawRoundedRect(finalRect, 3, 3);
+
+    QPixmap panda(":/basic/truthtable.svg");
+    QPointF pandaOrigin = finalRect.center();
+    pandaOrigin.setX(pandaOrigin.x() - panda.width() / 2);
+    pandaOrigin.setY(pandaOrigin.y() - panda.height() / 2);
+    tmpPainter.drawPixmap(pandaOrigin, panda);
+
+    // draw shadow
+    tmpPainter.setBrush(QColor(78, 78, 78));
+    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
+
+    QRectF shadowRect(finalRect.bottomLeft(), finalRect.bottomRight());
+    shadowRect.adjust(0, -3, 0, 0);
+    tmpPainter.drawRoundedRect(shadowRect, 3, 3);
+
+    // draw semicircle
+    QRectF topCenter = QRectF(finalRect.topLeft() + QPointF(18, -12), QSize(24, 24));
+    tmpPainter.drawChord(topCenter, 0, -180 * 16);
+
+    m_pixmap = std::make_unique<QPixmap>(tempPixmap);
+}
+
+
