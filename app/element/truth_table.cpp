0482ee64684af909d6f58342c370b384b0b16fd1
Matheus <matheus.resteves@gmail.com>
2024-03-15 16:28:56 -0300

    add paint function

--- app/element/truth_table.cpp
+++ app/element/truth_table.cpp
@@ -3,7 +3,9 @@
 
 #include "truth_table.h"
 
+#include "elementfactory.h"
 #include "globalproperties.h"
+#include "qneconnection.h"
 #include "qneport.h"
 
 namespace
@@ -26,6 +28,7 @@ TruthTable::TruthTable(QGraphicsItem *parent)
     setHasLabel(true);
 
     TruthTable::updatePortsProperties();
+    TruthTable::generatePixmap();
 }
 
 void TruthTable::updatePortsProperties()
@@ -71,3 +74,80 @@ void TruthTable::updatePortsProperties()
         }
     }
 }
+
+void TruthTable::generatePixmap()
+{
+    // make pixmap
+    const QSize size = portsBoundingRect().united(QRectF(0, 0, 64, 64)).size().toSize();
+    qDebug() << "Tamanho de portBounding: " << portsBoundingRect();
+    qDebug() << "Tamanho de size: " << size;
+
+    QPixmap tempPixmap(size);
+    qDebug() << "Tamanho de tempPixmap: " << tempPixmap;
+    tempPixmap.fill(Qt::transparent);
+
+    QPainter tmpPainter(&tempPixmap);
+
+    tmpPainter.setBrush(QColor(126, 126, 126));
+    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
+
+    // draw package
+    QPoint topLeft = tempPixmap.rect().topLeft();
+    topLeft.setX(topLeft.x() + 7);
+    QSize finalSize = tempPixmap.rect().size();
+    finalSize.setWidth(finalSize.width() - 14);
+    QRectF finalRect = QRectF(topLeft, finalSize);
+    tmpPainter.drawRoundedRect(finalRect, 3, 3);
+
+    QPixmap panda(":/basic/truthtable-rotated.svg");
+    QPointF pandaOrigin = finalRect.center();
+    pandaOrigin.setX(pandaOrigin.x() - panda.width() / 2);
+    pandaOrigin.setY(pandaOrigin.y() - panda.height() / 2);
+    tmpPainter.drawPixmap(pandaOrigin, panda);
+
+    // draw shadow
+    tmpPainter.setBrush(QColor(78, 78, 78));
+    tmpPainter.setPen(QPen(QBrush(QColor(78, 78, 78)), 0.5, Qt::SolidLine));
+
+    QRectF shadowRect(finalRect.bottomLeft(), finalRect.bottomRight());
+    shadowRect.adjust(0, -3, 0, 0);
+    tmpPainter.drawRoundedRect(shadowRect, 3, 3);
+
+    // draw semicircle
+    /*QRectF topCenter = QRectF(finalRect.topLeft() + QPointF(18, -12), QSize(24, 24));
+    tmpPainter.drawChord(topCenter, 0, -180 * 16);*/
+
+    m_pixmap = std::make_unique<QPixmap>(tempPixmap);
+
+}
+
+void TruthTable::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(widget)
+    Q_UNUSED(option)
+
+    TruthTable::updatePortsProperties();
+
+    if (isSelected()) {
+        painter->save();
+        painter->setBrush(m_selectionBrush);
+        painter->setPen(QPen(m_selectionPen, 0.5, Qt::SolidLine));
+        painter->drawRoundedRect(portsBoundingRect().united(QRectF(0, 0, 64, 64)), 5, 5);
+        painter->restore();
+    }
+
+    generatePixmap();
+    painter->drawPixmap(boundingRect().topLeft(), pixmap());
+    qDebug() << "Tamanho painter->drawRoundedRect: " << painter;
+
+}
+
+/*void TruthTable::update()
+{
+    TruthTable::updatePortsProperties();
+
+    const qreal bottom = portsBoundingRect().united(QRectF(0, 0, 64, 64)).bottom();
+    m_label->setPos(30, bottom + 5);
+
+    generatePixmap();
+}*/
