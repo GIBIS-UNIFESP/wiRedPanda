641514d744cb14a8d16f060d5ee3539bef73a221
Matheus <matheus.resteves@gmail.com>
2024-10-08 15:52:20 -0300

    Feat: add documentation in Temporal Simulation functions
    Co-Authored-By: Matheus Ramos Esteves <107145641+Esteves31@users.noreply.github.com>

--- app/logicelement.h
+++ app/logicelement.h
@@ -23,7 +23,13 @@ public:
     bool operator>(const LogicElement &other) const;
 
     bool inputValue(const int index = 0) const;
+    
+    /**
+     * Checks if Temporal Simulation is activated.
+     * @return True if Temporal Simulation is activated, and False if it's not activated.
+     */
     bool isTempSimulationOn();
+
     bool isValid() const;
     bool outputValue(const int index = 0) const;
     int calculatePriority();
@@ -32,8 +38,20 @@ public:
     void connectPredecessor(const int index, LogicElement *logic, const int port);
     void setOutputValue(const bool value);
     void setOutputValue(const int index, const bool value);
+
+    /**
+     * Set the m_TempSimulationIsOn to True or False.
+     * @param isOn is used to to knows which value will be used.
+     */
     void setTemporalSimulationIsOn(bool isOn);
+
+    /**
+     * Propagates the values in m_inputBuffer in each line to the next line,
+     * then needs delayLenght times to propagate the first line to final line,
+     * simulating the delay an each logic port.
+     */
     void updateInputBuffer();
+
     void validate();
 
 protected:
