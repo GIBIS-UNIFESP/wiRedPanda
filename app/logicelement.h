dea5e1e94da80339147e20d269d09a111f35e5de
Matheus <matheus.resteves@gmail.com>
2024-10-08 15:23:25 -0300

    Refactor: removing unused updateInputsInBuffer function
    Feat: add documentation in Temporal Simulation functions
    Co-Authored-By: Matheus Ramos Esteves <107145641+Esteves31@users.noreply.github.com>

--- app/logicelement.h
+++ app/logicelement.h
@@ -22,7 +22,13 @@ public:
     virtual ~LogicElement() = default;
 
     bool inputValue(const int index = 0) const;
+    
+    /**
+     * Checks if Temporal Simulation is activated.
+     * @return True if Temporal Simulation is activated, and False if it's not activated.
+     */
     bool isTempSimulationOn();
+
     bool isValid() const;
     bool outputValue(const int index = 0) const;
     int calculatePriority();
@@ -32,8 +38,20 @@ public:
     void connectPredecessor(const int index, LogicElement *logic, const int port);
     void setOutputValue(const bool value);
     void setOutputValue(const int index, const bool value);
+
+    /**
+     * Set the m_TempSimulationIsOn to True or False.
+     * @param isOn is used to to knows which value will be used.
+     */
     void setTemporalSimulationIsOn(bool isOn);
+
+    /**
+     * Propagates the values in m_inputBuffer in each line to the next line,
+     * then needs delayLenght times to propagate the first line to final line,
+     * simulating the delay an each logic port.
+     */
     void updateInputBuffer();
+
     void validate();
 
 protected:
@@ -41,12 +59,9 @@ protected:
 
     QVector<QVector<bool>> m_inputBuffer;
     QVector<bool> m_inputValues;
-    int m_delayLength;
 
 private:
     Q_DISABLE_COPY(LogicElement)
-
-    void updateInputsInBuffer();
 
     QVector<LogicElement *> m_successors;
     QVector<InputPair> m_inputPairs;
@@ -54,5 +69,6 @@ private:
     bool m_beingVisited = false;
     bool m_isValid = true;
     bool m_tempSimulationIsOn = false;
+    int m_delayCount = 0;
     int m_priority = -1;
 };
