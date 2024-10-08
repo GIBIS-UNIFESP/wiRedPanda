a5eb8e378e904f204571a94d44ec73b6994335ed
Matheus <matheus.resteves@gmail.com>
2024-10-08 15:15:58 -0300

    Style: put a zoom bar between Zoom In and Zoom Out

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -266,6 +266,7 @@ void MainWindow::createNewTab()
     m_ui->tab->setCurrentIndex(m_ui->tab->count() - 1);
 
     qCDebug(zero) << tr("Finished #tabs: ") << m_ui->tab->count() << tr(", current tab: ") << m_tabIndex;
+    m_ui->actionTemporalSimulation->setChecked(false);
 }
 
 void MainWindow::removeUndoRedoMenu()
@@ -781,6 +782,9 @@ void MainWindow::tabChanged(const int newTabIndex)
     qCDebug(zero) << tr("Selecting tab: ") << newTabIndex;
     connectTab();
     qCDebug(zero) << tr("New tab selected. Dolphin fileName: ") << m_currentTab->dolphinFileName();
+
+    auto isTempSimulation = m_currentTab->simulation()->isTemporalSimulation();
+    m_ui->actionTemporalSimulation->setChecked(isTempSimulation);
 }
 
 void MainWindow::on_lineEditSearch_textChanged(const QString &text)
