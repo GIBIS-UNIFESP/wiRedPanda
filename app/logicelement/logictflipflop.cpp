12af7b8412379a0bcb7ed510eb07bf8a81b03ed8
Matheus <matheus.resteves@gmail.com>
2024-10-08 15:17:45 -0300

    Fix: fixing the Temporal Simulation response in logic elements

--- app/logicelement/logictflipflop.cpp
+++ app/logicelement/logictflipflop.cpp
@@ -17,31 +17,31 @@ void LogicTFlipFlop::updateLogic()
     }
 
     if (isTempSimulationOn()) {
-        bool q0 = outputValue(0);
-        bool q1 = outputValue(1);
-        const bool T = m_inputBuffer.last()[0];
-        const bool clk = m_inputBuffer.last()[1];
-        const bool prst = m_inputBuffer.last()[2];
-        const bool clr = m_inputBuffer.last()[3];
+            bool q0 = outputValue(0);
+            bool q1 = outputValue(1);
+            const bool T = m_inputBuffer.last()[0];
+            const bool clk = m_inputBuffer.last()[1];
+            const bool prst = m_inputBuffer.last()[2];
+            const bool clr = m_inputBuffer.last()[3];
 
-        if (clk && !m_lastClk) {
-            if (m_lastValue) {
-                q0 = !q0;
-                q1 = !q0;
+            if (clk && !m_lastClk) {
+                if (m_lastValue) {
+                    q0 = !q0;
+                    q1 = !q0;
+                }
             }
-        }
 
-        if (!prst || !clr) {
-            q0 = !prst;
-            q1 = !clr;
-        }
+            if (!prst || !clr) {
+                q0 = !prst;
+                q1 = !clr;
+            }
 
-        m_lastClk = clk;
-        m_lastValue = T;
+            m_lastClk = clk;
+            m_lastValue = T;
 
-        setOutputValue(0, q0);
-        setOutputValue(1, q1);
-        updateInputBuffer();
+            setOutputValue(0, q0);
+            setOutputValue(1, q1);
+            updateInputBuffer();
     } else {
         bool q0 = outputValue(0);
         bool q1 = outputValue(1);
