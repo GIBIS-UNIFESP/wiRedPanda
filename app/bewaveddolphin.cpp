99fc6de9aeffd58cfbaa58dc5c3a4c25f2058b26
Matheus <matheus.resteves@gmail.com>
2024-10-09 13:52:02 -0300

    Feat: removing the selection click on the output cells in dolphin

--- app/bewaveddolphin.cpp
+++ app/bewaveddolphin.cpp
@@ -38,9 +38,13 @@ SignalModel::SignalModel(const int inputs, const int rows, const int columns, QO
 
 Qt::ItemFlags SignalModel::flags(const QModelIndex &index) const
 {
-    Q_UNUSED(index)
+    //Q_UNUSED(index)
+    Qt::ItemFlags flags;
 
-    Qt::ItemFlags flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+    if (index.row() >= m_inputCount)
+        flags = Qt::ItemIsEnabled;
+    else
+        flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 
     return flags;
 }
@@ -1540,7 +1544,6 @@ void BewavedDolphin::createTemporalSimulationElement(const int row, const int co
         m_model->setData(index, Qt::AlignLeft, Qt::TextAlignmentRole);
 
         m_model->setData(index, composedWaveForm, Qt::DecorationRole);
-
         if (!changeNext) {
             return;
         }
