dc7a62c823f2ed94484f90b2c693f03c1694bc12
Matheus <matheus.resteves@gmail.com>
2024-10-10 15:04:38 -0300

    Fix: add previousWaveEnd parameter in functions to create waveform in Temporal Simulation for create corectly waveforms.

--- app/bewaveddolphin.h
+++ app/bewaveddolphin.h
@@ -93,7 +93,7 @@ private:
      * @param lastValue is used to knows the last state to create the first state of the current waveform.
      * @return a Pixmap containing the waveform image 64x38 pixels.
     */
-    QPixmap composeWaveParts(const QVector<bool> waveparts, const bool isInput = true);
+    QPixmap composeWaveParts(const QVector<bool> waveparts, std::optional<bool> previousWaveEnd, const bool isInput = true);
 
     /**
      * This function converts a boolean vector into a string of hexadecimal values.
@@ -131,7 +131,7 @@ private:
      * @param isInput is True if is about an input waveform or False if you want an output waveform.
      * @param changeNext is True if you want to change next value in the row, and False if you don't.
     */
-    void createComposedWaveFormElement(const int row, const int column, QVector<bool> output, const bool isInput = true, const bool changeNext = true);
+    void createComposedWaveFormElement(const int row, const int column, QVector<bool> output, std::optional<bool> previousWaveEnd, const bool isInput = true, const bool changeNext = true);
 
     void cut(const QItemSelection &ranges, QDataStream &stream);
     void load(QDataStream &stream);
