aa83189c8aa75c3fe4da8aa442186ecdc33ed8c0
Matheus <matheus.resteves@gmail.com>
2024-10-10 15:37:15 -0300

    Fix: including the <optional> into bewavedolphin.cpp

--- app/bewaveddolphin.cpp
+++ app/bewaveddolphin.cpp
@@ -31,6 +31,7 @@
 #include <bitset>
 #include <cmath>
 #include <iostream>
+#include <optional>
 #include <sstream>
 
 SignalModel::SignalModel(const int inputs, const int rows, const int columns, QObject *parent)
