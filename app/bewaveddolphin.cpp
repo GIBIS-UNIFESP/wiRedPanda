6e9abe42ab8719f3cf1b4da4a8b164db6c71c141
Matheus <matheus.resteves@gmail.com>
2024-10-10 15:02:08 -0300

    Fix: the first position in the composed waveform element does not consider the last position in the last composed waveform element in Temporal Simulation.

--- app/bewaveddolphin.cpp
+++ app/bewaveddolphin.cpp
@@ -425,6 +425,7 @@ void BewavedDolphin::run2()
     SimulationBlocker simulationBlocker(m_simulation);
 
     int nPorts = 0;
+    std::optional<bool> previousWaveEnd = NULL;
 
     for (auto *output : qAsConst(m_outputs)) {
         nPorts += output->inputSize();
@@ -465,7 +466,8 @@ void BewavedDolphin::run2()
             row = m_inputPorts;
 
             for (const auto& output : output_values) {
-                createComposedWaveFormElement(row, column, output, false);
+                createComposedWaveFormElement(row, column, output, previousWaveEnd, false);
+                previousWaveEnd = output.last();
                 row++;
             }
         }
@@ -490,9 +492,9 @@ void BewavedDolphin::run2()
     restoreInputs();
 }
 
-void BewavedDolphin::createComposedWaveFormElement(const int row, const int column, QVector<bool> output, const bool isInput, const bool changeNext)
+void BewavedDolphin::createComposedWaveFormElement(const int row, const int column, QVector<bool> output, std::optional<bool> previousWaveEnd, const bool isInput, const bool changeNext)
 {
-    QPixmap composedWave = composeWaveParts(output, isInput);
+    QPixmap composedWave = composeWaveParts(output, previousWaveEnd, isInput);
     const std::string hex = convertBinaryToHex(output);
     createTemporalSimulationElement(row, column, composedWave, hex, isInput, changeNext);
 }
@@ -1436,7 +1438,7 @@ void BewavedDolphin::on_actionTemporalSimulation_toggled(const bool checked)
     m_isTemporalSimulation = checked;
 }
 
-QPixmap BewavedDolphin::composeWaveParts(const QVector<bool> waveparts, const bool isInput)
+QPixmap BewavedDolphin::composeWaveParts(const QVector<bool> waveparts, std::optional<bool> previousWaveEnd, const bool isInput)
 {
     int partWidth = 64;
     int partHeight = 38;
@@ -1471,12 +1473,20 @@ QPixmap BewavedDolphin::composeWaveParts(const QVector<bool> waveparts, const bo
 
         if (i == 0) {
             if (waveparts[i] == true) {
-                painter.drawPixmap(x, 0, high);
+                if (previousWaveEnd == false) {
+                    painter.drawPixmap(x, 0, rising);
+                } else {
+                    painter.drawPixmap(x, 0, high);
+                }
                 previousIsLow = false;
                 previousIsHigh = true;
             }
             else {
-                painter.drawPixmap(x, 0, low);
+                if (previousWaveEnd == true) {
+                    painter.drawPixmap(x, 0, falling);
+                } else {
+                    painter.drawPixmap(x, 0, low);
+                }
                 previousIsLow = true;
                 previousIsHigh = false;
             }
