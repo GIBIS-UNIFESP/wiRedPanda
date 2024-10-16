6b7710ad20c2580764a526bd1740532b1fca5b0f
Matheus <matheus.resteves@gmail.com>
2024-10-16 19:49:20 -0300

    Fix: fixed merge with Temporal Simulation branch

--- app/elementeditor.cpp
+++ app/elementeditor.cpp
@@ -888,7 +888,6 @@ void ElementEditor::truthTable()
     for (int i = 0; i < pow(2, nInputs); i++) {
         for (int j = 0; j < nInputs; j++) {
             m_table->setColumnWidth(j,14);
-            //m_ui->truthTable->setColumnWidth(j,14);
             auto newItemValue = QString::number(i, 2);
 
             if (newItemValue.size() < nInputs) {
@@ -937,17 +936,11 @@ void ElementEditor::setTruthTableProposition(const int row, const int column)
         return;
     }
 
-<<<<<<< HEAD
     auto cellItem = m_table->item(row,column);
     cellItem->setText((cellItem->text() == "0") ? "1" : "0");
 
     auto truthtable = m_elements[0];
     const int nInputs = truthtable->inputSize();
-=======
-    auto *truthtable = m_elements[0];
-
-    auto *cellItem = m_ui->truthTable->item(row,column);
->>>>>>> temporal_simulation
 
     const QString newItemValue = (cellItem->text() == "0") ? "1" : "0";
 
