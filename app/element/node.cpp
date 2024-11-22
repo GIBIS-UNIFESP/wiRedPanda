a33a5864076995f8ea65ec1bcb8fac92d4c0ff0e
mams76 <maycon.andre@unifesp.br>
2024-11-22 16:44:58 -0300

    Fix panda name in branch

--- app/element/node.cpp
+++ app/element/node.cpp
@@ -1,4 +1,4 @@
-// Copyright 2015 - 2024, GIBIS-UNIFESP and the WiRedPanda contributors
+// Copyright 2015 - 2024, GIBIS-UNIFESP and the wiRedPanda contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "node.h"
