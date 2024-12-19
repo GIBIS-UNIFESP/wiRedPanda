7a1a8b48ba5b1143532d653194a07d5904ccafe0
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-12-19 16:05:12 -0300

    Change updateLogic() implementation
    Co-Authored-By: Matheus Ramos Esteves <107145641+Esteves31@users.noreply.github.com>

--- app/logicelement/logicdflipflop.cpp
+++ app/logicelement/logicdflipflop.cpp
@@ -4,7 +4,7 @@
 #include "logicdflipflop.h"
 
 LogicDFlipFlop::LogicDFlipFlop()
-    : LogicElement(4, 2)
+    : LogicElement(4, 2, 13)
 {
     setOutputValue(0, false);
     setOutputValue(1, true);
@@ -16,26 +16,52 @@ void LogicDFlipFlop::updateLogic()
         return;
     }
 
-    bool q0 = outputValue(0);
-    bool q1 = outputValue(1);
-    const bool D = m_inputValues.at(0);
-    const bool clk = m_inputValues.at(1);
-    const bool prst = m_inputValues.at(2);
-    const bool clr = m_inputValues.at(3);
+    if (isTempSimulationOn()) {
+        bool q0 = outputValue(0);
+        bool q1 = outputValue(1);
+        const bool D = m_inputBuffer.last()[0];
+        const bool clk = m_inputBuffer.last()[1];
+        const bool prst = m_inputBuffer.last()[2];
+        const bool clr = m_inputBuffer.last()[3];
 
-    if (clk && !m_lastClk) {
-        q0 = m_lastValue;
-        q1 = !m_lastValue;
+        if (clk && !m_lastClk) {
+            q0 = m_lastValue;
+            q1 = !m_lastValue;
+        }
+
+        if (!prst || !clr) {
+            q0 = !prst;
+            q1 = !clr;
+        }
+
+        m_lastClk = clk;
+        m_lastValue = D;
+
+        setOutputValue(0, q0);
+        setOutputValue(1, q1);
+        updateInputBuffer();
+    } else {
+        bool q0 = outputValue(0);
+        bool q1 = outputValue(1);
+        const bool D = m_inputValues.at(0);
+        const bool clk = m_inputValues.at(1);
+        const bool prst = m_inputValues.at(2);
+        const bool clr = m_inputValues.at(3);
+
+        if (clk && !m_lastClk) {
+            q0 = m_lastValue;
+            q1 = !m_lastValue;
+        }
+
+        if (!prst || !clr) {
+            q0 = !prst;
+            q1 = !clr;
+        }
+
+        m_lastClk = clk;
+        m_lastValue = D;
+
+        setOutputValue(0, q0);
+        setOutputValue(1, q1);
     }
-
-    if (!prst || !clr) {
-        q0 = !prst;
-        q1 = !clr;
-    }
-
-    m_lastClk = clk;
-    m_lastValue = D;
-
-    setOutputValue(0, q0);
-    setOutputValue(1, q1);
 }
