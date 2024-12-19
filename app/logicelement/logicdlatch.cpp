3fd89c959ed869ca2802dc9be3cf03bc6f2fc6cf
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-12-19 16:05:12 -0300

    Change updateLogic() implementation
    Co-Authored-By: Matheus Ramos Esteves <107145641+Esteves31@users.noreply.github.com>

--- app/logicelement/logicdlatch.cpp
+++ app/logicelement/logicdlatch.cpp
@@ -4,7 +4,7 @@
 #include "logicdlatch.h"
 
 LogicDLatch::LogicDLatch()
-    : LogicElement(2, 2)
+    : LogicElement(2, 2, 7)
 {
     setOutputValue(0, false);
     setOutputValue(1, true);
@@ -16,16 +16,32 @@ void LogicDLatch::updateLogic()
         return;
     }
 
-    bool q0 = outputValue(0);
-    bool q1 = outputValue(1);
-    const bool D = m_inputValues.at(0);
-    const bool enable = m_inputValues.at(1);
+    if (isTempSimulationOn()) {
+        bool q0 = outputValue(0);
+        bool q1 = outputValue(1);
+        const bool D = m_inputBuffer.last()[0];
+        const bool enable = m_inputBuffer.last()[1];
 
-    if (enable) {
-        q0 = D;
-        q1 = !D;
+        if (enable) {
+            q0 = D;
+            q1 = !D;
+        }
+
+        setOutputValue(0, q0);
+        setOutputValue(1, q1);
+        updateInputBuffer();
+    } else {
+        bool q0 = outputValue(0);
+        bool q1 = outputValue(1);
+        const bool D = m_inputValues.at(0);
+        const bool enable = m_inputValues.at(1);
+
+        if (enable) {
+            q0 = D;
+            q1 = !D;
+        }
+
+        setOutputValue(0, q0);
+        setOutputValue(1, q1);
     }
-
-    setOutputValue(0, q0);
-    setOutputValue(1, q1);
 }
