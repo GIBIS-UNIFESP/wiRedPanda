c313701a07cbd47efad5be8f03f719aa783c36db
Maycon <mayconandre75@gmail.com>
2025-04-24 01:09:35 -0300

    add tooltip

--- app/locateicbutton.cpp
+++ app/locateicbutton.cpp
@@ -6,5 +6,6 @@
 LocateIcButton::LocateIcButton(QWidget *parent)
     : QPushButton(QIcon(":/toolbar/folder.svg"), "", parent)
 {
+    setToolTip("Search for IC");
 }
 
--- app/removeicbutton.cpp
+++ app/removeicbutton.cpp
@@ -6,4 +6,5 @@
 RemoveIcButton::RemoveIcButton(QWidget *parent)
     : QPushButton(QIcon(":/toolbar/trashcan.svg"), "", parent)
 {
+    setToolTip("Delete IC from global list");
 }
