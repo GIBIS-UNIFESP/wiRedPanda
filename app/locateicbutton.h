9ebe30ec4d9f0da4d2729727cb3843d8956b5342
Maycon <mayconandre75@gmail.com>
2025-04-24 00:20:47 -0300

    fix button layout

---
+++ app/locateicbutton.h
@@ -0,0 +1,14 @@
+// Copyright 2015 - 2025, GIBIS-UNIFESP and the wiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <QPushButton>
+
+class LocateIcButton : public QPushButton
+{
+    Q_OBJECT
+
+public:
+    explicit LocateIcButton(QWidget *parent = nullptr);
+};
---
+++ app/removeicbutton.cpp
@@ -0,0 +1,9 @@
+// Copyright 2015 - 2025, GIBIS-UNIFESP and the wiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "removeicbutton.h"
+
+RemoveIcButton::RemoveIcButton(QWidget *parent)
+    : QPushButton(QIcon(":/toolbar/trashcan.svg"), "", parent)
+{
+}
