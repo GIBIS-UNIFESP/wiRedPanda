cb9448b1654eb317655fb2ed5ad76815e1e2a923
joao-zip <oliveira.joao@unifesp.br>
2026-03-06 16:23:05 -0300

    refactor: complete tailwindcss refactor, and dark/white mode addition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://gibis-unifesp.github.io/wiRedPanda',
   base: '/wiRedPanda/',
-  integrations: [icon()]
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
