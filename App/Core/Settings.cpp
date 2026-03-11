bf630c293797fbc3013d3c9a27f739ed7cd61963
Rodrigo Torres <torres.dark@gmail.com>
2026-03-11 11:01:03 -0300

    feat: Add copyright headers to source files missing them
    Add GPL-3.0-or-later copyright and SPDX license identifiers to files
    across App/ and root that were missing headers. This includes:
    - Core module files (Application, Enums, Settings, RegisterTypes)
    - UI module files (TrashButton)
    - Root precompiled header (pch.h)

--- App/Core/Settings.cpp
+++ App/Core/Settings.cpp
@@ -1,3 +1,6 @@
+// Copyright 2015 - 2025, GIBIS-UNIFESP and the wiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "Settings.h"
 
 QSettings *Settings::settingsInstance()
--- pch.h
+++ pch.h
@@ -1,3 +1,6 @@
+// Copyright 2015 - 2025, GIBIS-UNIFESP and the wiRedPanda contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #if defined __cplusplus
 // Qt includes (most common and stable)
 #include <QBitArray>
