6153f7ddb31544163f34fe60a5cb90a0e2270863
Rodrigo Torres <torres.dark@gmail.com>
2026-04-05 18:56:44 -0300

    refactor: use = default for MainWindow destructor

--- App/UI/MainWindow.cpp
+++ App/UI/MainWindow.cpp
@@ -367,9 +367,7 @@ void MainWindow::connectSceneAction(QAction *action, void (Scene::*method)())
     });
 }
 
-MainWindow::~MainWindow()
-{
-}
+MainWindow::~MainWindow() = default;
 
 void MainWindow::loadAutosaveFiles()
 {
